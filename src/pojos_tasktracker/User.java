de1790c75c3187f46c387b26e70464775b5ddc31
edatkins2 <edatkins2@LAPTOP-TRF623FP.fec.org>
2017-12-15 14:34:08 -0600

    TaskTracker Pojo

---
+++ src/pojos_tasktracker/User.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+/**
+ * @author edatkins2
+ *
+ */
+package pojos_tasktracker;
+
+public class User {
+
+	private Long id;
+	private String username;
+	private String pass;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return username;
+	}
+
+	public void setUserName(String userName) {
+		this.username = userName;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	}
